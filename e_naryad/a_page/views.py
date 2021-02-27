d61f5b61c97611c0a1677c3fb8ec1405cb51871d
Aksaule <aksaule124026@gmail.com>
2021-02-27 19:28:07 +0600

    Steps

--- e_naryad/a_page/views.py
+++ e_naryad/a_page/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def page1(request):
+    return render(request, 'page1.html')
+
+def enar(request):
+    return render(request, 'enar.html')
