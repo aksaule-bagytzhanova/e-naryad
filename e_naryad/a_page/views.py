56863d1f0ed4df3068dd4cb6a22df01b9831a556
Aksaule <aksaule124026@gmail.com>
2021-03-28 16:56:19 +0600

    Problems with Python versions and Django. More work with templates. More work with urls

--- e_naryad/a_page/views.py
+++ e_naryad/a_page/views.py
@@ -6,3 +6,15 @@ def page1(request):
 
 def enar(request):
     return render(request, 'enar.html')
+
+def create_nar_page1(request):
+    return render(request, 'create_nar_page1.html')
+
+def open_nar_page1(request):
+    return render(request, 'open_nar_page1.html')
+
+def close_nar_page1(request):
+    return render(request, 'close_nar_page1.html')
+
+def order_index(request):
+    return render(request, 'order_index.html') 
