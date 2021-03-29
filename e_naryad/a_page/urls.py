d8aa76e2d5488a976a70d86ae6db0dc65215e783
Aksaule <aksaule124026@gmail.com>
2021-03-29 11:51:00 +0600

    Fix sidebar

--- e_naryad/a_page/urls.py
+++ e_naryad/a_page/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('create_nar', views.create_nar_page1, name='create_nar_page1'),
     path('open_nar', views.open_nar_page1, name='open_nar_page1'),
     path('close_nar', views.close_nar_page1, name='close_nar_page1'),
-    path('order_index', views.order_index, name='order_index'),
+    path('order_index', views.order_index, name='order_i'),
     
     
 ]
