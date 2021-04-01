0b4b2e735734df85ba8373ee969329978405b6ca
Aksaule <aksaule124026@gmail.com>
2021-04-01 16:17:08 +0600

    Create db models

--- e_naryad/a_page/models.py
+++ e_naryad/a_page/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Employee(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    surname = models.CharField(max_length=200, null=True)
+    position = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name  
+
+
+
+
