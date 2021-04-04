510d09edb59108830ea25f8ceb59fd4f42dfb9b7
180107062 <62544970+Flip7418@users.noreply.github.com>
2021-04-04 20:54:12 +0600

    Update 18.sql

--- SQLite Commands/18.sql
+++ SQLite Commands/18.sql
@@ -1,1 +1,1 @@
-Select from Employee_add_holiday LEFT join Employee_time on Employee_add_gate.Employee_id=Employee_time.Employee_id where Holiday_Hours>0;
+Select from Employee_add_holiday LEFT join Employee_time on Employee_add_holiday.Employee_id=Employee_time.Employee_id where Holiday_Hours>0;
