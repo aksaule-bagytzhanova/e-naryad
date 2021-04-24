2e1c20263ad142519a62d2285180fd0a4c725d51
Aksaule <aksaule124026@gmail.com>
2021-04-24 17:40:26 +0600

    Some changes to users info

--- e_naryad/a_page/models.py
+++ e_naryad/a_page/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.db.models.base import Model
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Employee(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     username = models.CharField(max_length=200, null=True)
     password = models.CharField(max_length=200, null=True)
     name = models.CharField(max_length=200, null=True)
